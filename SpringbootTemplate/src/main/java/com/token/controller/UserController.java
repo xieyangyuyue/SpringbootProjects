0e4fcf2b2196112a26149d23746af8290ff3eea0
xieyangyuyue <1483618794@qq.com>
2024-10-17 16:02:09 +0800

    制作模板

---
+++ SpringbootTemplate/src/main/java/com/token/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.token.controller;
+
+import com.token.pojo.User;
+import com.token.service.UserService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+public class UserController {
+    @Resource
+    private UserService userService;
+	 @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+    //返回所有数据
+    @GetMapping("/list")
+    public List<User> list() {
+        return userService.listAll();
+    }
+
+    //表单新增 将表单数据写入数据库
+    @PostMapping("/save")
+    public Result save(@RequestBody User user) {
+        return userService.save(user) ? Result.success() : Result.fail();
+    }
+
+    //更新
+    @PostMapping("/update")
+    public Result update(@RequestBody User user) {
+        return userService.updateById(user) ? Result.success() : Result.fail();
+    }
+
+    //删除
+    @GetMapping("/delete")
+    public Result delete(@RequestParam Integer id) {
+        return userService.removeById(id) ? Result.success() : Result.fail();
+    }
+
+    //新增或修改
+    @PostMapping("/saveOrMod")
+    public Result saveOrMod(@RequestBody User user) {
+        return userService.saveOrUpdate(user) ? Result.success() : Result.fail();
+    }
+
+}
